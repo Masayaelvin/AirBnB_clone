94d395262644695ac1c7ac084f9a2262ab65bdc0
Opiyo24 <gabbynyangor@gmail.com>
2023-10-10 18:49:04 +0300

    Base_model_from_dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -3,20 +3,39 @@ import datetime
 import uuid
 
 class BaseModel:
+        
     id = uuid.uuid4()
     created_at = datetime.datetime.now()
     updated_at = datetime.datetime.now()
+
+    def __init__(self, *args, **kwargs):
+        #if kwargs is npot empty
+        if kwargs and kwargs != None:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+
+        #otherwise
+        if "id" not in kwargs.keys():
+            self.id = uuid.uuid4()
+        if "created_at" not in kwargs.keys():
+            self.created_at = datetime.datetime.now()
+        if "updated_at" not in kwargs.keys():
+            self.updated_at = datetime.datetime.now()
+
 
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
     
     def save(self):
         self.updated_at = datetime.datetime.now()
-        return (self.updated_at) #we come comeback
-
+    
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        print(obj_dict)
+        return(obj_dict)
