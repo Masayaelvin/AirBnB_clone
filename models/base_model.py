ed1cd9cedd7ac6c3006737bebe81fdb5774f68a7
Masayaelvin <masayaelvin@gmail.com>
2023-10-10 03:30:38 -0700

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import datetime
+import uuid
+
+class BaseModel:
+    id = uuid.uuid4()
+    created_at = datetime.datetime.now()
+    updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+    
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+        return (self.updated_at) #we come comeback
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        print(obj_dict)
