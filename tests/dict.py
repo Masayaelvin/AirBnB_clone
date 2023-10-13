17d25e36ed93787d08c2d4098ab9e002731f7de5
Masayaelvin <masayaelvin@gmail.com>
2023-10-13 08:25:40 +0300

    stage commit

---
+++ tests/dict.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# from models import storage
+from models.base_model import BaseModel
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
